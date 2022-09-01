98f577773560bb8ee9c1386fe24eed150203b99e
Guilherme Castro <guilherme.castro.souza1999@gmail.com>
2022-09-01 14:51:42 -0400

    adicionando posts a pagina inicial

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
+import sqlite3
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    con = get_db()
+    posts = con.execute('SELECT * FROM posts').fetchall()
+    con.close()
+    return render_template('index.html', posts=posts)
+
+
+def get_db():
+    con = sqlite3.connect('db.sqlite3')
+    con.row_factory = sqlite3.Row
+    
+    return con

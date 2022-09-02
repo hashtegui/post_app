b365a1cae116625c4d0691172136ef3a3f5b6998
Guilherme Castro <guilherme.castro.souza1999@gmail.com>
2022-09-01 20:34:34 -0400

    construindo pagina de posts

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import sqlite3
+from werkzeug.exceptions import abort
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
@@ -11,8 +13,23 @@ def index():
     return render_template('index.html', posts=posts)
 
 
+@app.route('/<int:post_id>')
+def post(post_id):
+    post = get_post(post_id)
+    return render_template('post.html', post=post)
+
+
 def get_db():
     con = sqlite3.connect('db.sqlite3')
     con.row_factory = sqlite3.Row
-    
+
     return con
+
+
+def get_post(post_id:int):
+    con = get_db()
+    post = con.execute('SELECT * FROM posts where id = ?', (post_id,)).fetchone()
+    con.close()
+    if post is None:
+        abort(404)
+    return post
